cc193e45bcd607bdc30d7d868a65c2939f27b04a
JC Castagne <74418183+JCcastagne@users.noreply.github.com>
2022-02-22 00:08:13 -0500

    start creating flatList for feed view (loading dynamicaly from data)

--- screens/NewsFeed.js
+++ screens/NewsFeed.js
@@ -11,6 +11,8 @@ import {
 } from 'react-native'
 import { useBottomTabBarHeight } from '@react-navigation/bottom-tabs'
 import { MaterialCommunityIcons } from '@expo/vector-icons'
+import { FlatList } from 'react-native'
+import { Roboto_400Regular } from '@expo-google-fonts/roboto'
 
 const posts = [
   {
@@ -43,20 +45,6 @@ const posts = [
 export default function NewsFeed () {
   const tabBarHeight = useBottomTabBarHeight()
 
-  function chooseBuilding () {
-    Alert.alert(
-      'Notice',
-      'This is where we would redirect to building info screen',
-      [{ text: 'OK', onPress: () => console.log('OK Pressed') }]
-    )
-  }
-  function clickAnnouncements () {
-    Alert.alert(
-      'Notice',
-      'This is where we would redirect to announcements screen',
-      [{ text: 'OK', onPress: () => console.log('OK Pressed') }]
-    )
-  }
   function clickPost () {
     Alert.alert('Notice', `This is where we would redirect to post's screen`, [
       { text: 'OK', onPress: () => console.log('OK Pressed') }
@@ -65,122 +53,48 @@ export default function NewsFeed () {
 
   return (
     <SafeAreaView style={{ backgroundColor: '#395E66' }}>
-      <View
-        id='header'
-        style={{
-          backgroundColor: '#395E66',
-          paddingHorizontal: 17,
-          paddingTop: Platform.OS === 'ios' ? 0 : 34
-        }}
-      >
-        <Text
-          id='screenName'
-          style={{
-            fontFamily: 'Roboto_700Bold',
-            color: '#FFFFFF',
-            fontSize: 34,
-            paddingTop: 11
-          }}
-        >
-          NewsFeed
-        </Text>
-        <Pressable id='currentBuilding' onPress={chooseBuilding}>
-          <Text
-            style={{
-              fontFamily: 'Roboto_400Regular',
-              color: '#FFFFFFA8',
-              fontSize: 14,
-              paddingTop: 7,
-              paddingBottom: 17
-            }}
-          >
-            123 Robinson Rd, Ottawa, ON
-            <MaterialCommunityIcons
-              name='chevron-right'
-              size={14}
-              color='#FFFFFFA8'
-            />
+      {!posts && (
+        <View style={{ flex: 1 }}>
+          <Text style={{ fontSize: 22, fontFamily: 'Roboto_400Regular' }}>
+            Loading...
           </Text>
-        </Pressable>
-      </View>
+        </View>
+      )}
 
-      <View
-        id='feed'
-        style={{
-          backgroundColor: '#FFFFFF',
-          borderTopRightRadius: 27,
-          borderTopLeftRadius: 27
-        }}
-      >
-        {/* //announcements */}
-        <Pressable
-          id='announcements'
-          onPress={clickAnnouncements}
+      {posts && (
+        <FlatList
+          id='feed'
           style={{
-            flexDirection: 'row',
-            alignItems: 'center',
-            justifyContent: 'space-between',
             backgroundColor: '#FFFFFF',
-            paddingHorizontal: 22,
-            paddingVertical: 17,
-            marginHorizontal: 17,
-            marginTop: 17,
-            marginBottom: 13,
-
-            borderRadius: 24,
-            shadowColor: 'black',
-            shadowOffset: { width: 0, height: -8 },
-            shadowOpacity: 0.14,
-            shadowRadius: 34,
-            elevation: 16
+            borderTopRightRadius: 27,
+            borderTopLeftRadius: 27
           }}
-        >
-          <Text
-            id='title'
-            style={{
-              fontFamily: 'Roboto_500Medium',
-              fontSize: 22,
-              color: '#395E66'
-            }}
-          >
-            Announcements
-          </Text>
-          <View
-            id='indice'
-            style={{
-              flexDirection: 'row',
-              alignItems: 'center'
-            }}
-          >
+          ListHeaderComponent={<HeaderAnnouncements />}
+          data={posts}
+          keyExtractor={(item, index) => {
+            return item + '-' + index
+          }}
+          renderItem={({ item }) => {
+            return (
+              <Post
+                name={item.name}
+                // id={post.id}
+                // avatar={post.avatar}
+                // name={post.name}
+                // timestamp={post.timestamp}
+                // message={post.message}
+              />
+            )
+          }}
+          ListFooterComponent={
             <View
-              id='counterBadge'
-              style={{
-                backgroundColor: '#395E66',
-                paddingHorizontal: 8,
-                paddingVertical: 1,
-                borderRadius: 99
-              }}
-            >
-              <Text
-                id='counter'
-                style={{
-                  fontFamily: 'Roboto_500Medium',
-                  fontSize: 15,
-                  color: '#FFFFFF',
-                  lineHeight: 22
-                }}
-              >
-                2
-              </Text>
-            </View>
-            <MaterialCommunityIcons
-              name='chevron-right'
-              size={36}
-              color='black'
-            />
-          </View>
-        </Pressable>
+              style={{ paddingVertical: useBottomTabBarHeight + 34 }}
+            ></View>
+          }
+        />
+      )}
 
+      <View>
         {/* //post */}
         <Pressable
           id='post'
@@ -488,5 +402,143 @@ export default function NewsFeed () {
         <MaterialCommunityIcons name='plus' size={36} color='white' />
       </Pressable>
     </SafeAreaView>
+  )
+}
+
+function HeaderAnnouncements () {
+  const tabBarHeight = useBottomTabBarHeight()
+  function chooseBuilding () {
+    Alert.alert(
+      'Notice',
+      'This is where we would redirect to building info screen',
+      [{ text: 'OK', onPress: () => console.log('OK Pressed') }]
+    )
+  }
+  function clickAnnouncements () {
+    Alert.alert(
+      'Notice',
+      'This is where we would redirect to announcements screen',
+      [{ text: 'OK', onPress: () => console.log('OK Pressed') }]
+    )
+  }
+
+  return (
+    <View>
+      <View
+        id='header'
+        style={{
+          backgroundColor: '#395E66',
+          paddingHorizontal: 17,
+          paddingTop: Platform.OS === 'ios' ? 0 : 34
+        }}
+      >
+        <Text
+          id='screenName'
+          style={{
+            fontFamily: 'Roboto_700Bold',
+            color: '#FFFFFF',
+            fontSize: 34,
+            paddingTop: 11
+          }}
+        >
+          NewsFeed
+        </Text>
+        <Pressable id='currentBuilding' onPress={chooseBuilding}>
+          <Text
+            style={{
+              fontFamily: 'Roboto_400Regular',
+              color: '#FFFFFFA8',
+              fontSize: 14,
+              paddingTop: 7,
+              paddingBottom: 17
+            }}
+          >
+            123 Robinson Rd, Ottawa, ON
+            <MaterialCommunityIcons
+              name='chevron-right'
+              size={14}
+              color='#FFFFFFA8'
+            />
+          </Text>
+        </Pressable>
+      </View>
+
+      {/* //announcements */}
+      <Pressable
+        id='announcements'
+        onPress={clickAnnouncements}
+        style={{
+          flexDirection: 'row',
+          alignItems: 'center',
+          justifyContent: 'space-between',
+          backgroundColor: '#FFFFFF',
+          paddingHorizontal: 22,
+          paddingVertical: 17,
+          marginHorizontal: 17,
+          marginTop: 17,
+          marginBottom: 13,
+
+          borderRadius: 24,
+          shadowColor: 'black',
+          shadowOffset: { width: 0, height: -8 },
+          shadowOpacity: 0.14,
+          shadowRadius: 34,
+          elevation: 16
+        }}
+      >
+        <Text
+          id='title'
+          style={{
+            fontFamily: 'Roboto_500Medium',
+            fontSize: 22,
+            color: '#395E66'
+          }}
+        >
+          Announcements
+        </Text>
+        <View
+          id='indice'
+          style={{
+            flexDirection: 'row',
+            alignItems: 'center'
+          }}
+        >
+          <View
+            id='counterBadge'
+            style={{
+              backgroundColor: '#395E66',
+              paddingHorizontal: 8,
+              paddingVertical: 1,
+              borderRadius: 99
+            }}
+          >
+            <Text
+              id='counter'
+              style={{
+                fontFamily: 'Roboto_500Medium',
+                fontSize: 15,
+                color: '#FFFFFF',
+                lineHeight: 22
+              }}
+            >
+              2
+            </Text>
+          </View>
+          <MaterialCommunityIcons
+            name='chevron-right'
+            size={36}
+            color='black'
+          />
+        </View>
+      </Pressable>
+    </View>
+  )
+}
+
+function Post ({ name }) {
+  return (
+    <View>
+      <Text>{name}</Text>
+    </View>
   )
 }
